b191e4e5533acebf599c319966f5aaf3e0765773
Pond666 <suphakitpyto3321@gmail.com>
2021-12-17 14:30:18 +0700

    first commit

---
+++ src/main/java/nvc/it/trash/TrashApplication.java
@@ -0,0 +1,13 @@
+package nvc.it.trash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashApplication.class, args);
+	}
+
+}
